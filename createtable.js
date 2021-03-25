c36659b7330711997b6c884c0a0b05d925d2d7d7
YisakeBeyene <tadeleyisake@gmail.com>
2021-03-24 23:12:33 -0400

    Random box, and response after timer done

--- createtable.js
+++ createtable.js
@@ -1,20 +1,26 @@
-function getInputValue() {
-  var inputVal = document.getElementById("myInput").value;
-  var table_size = inputVal;
-  document.cookie = table_size;
-  sessionStorage.setItem("table_size", table_size);
-}
-
-
 function generate_table() {
     // get the reference for the body
     var body = document.getElementsByTagName("body")[0];
     
     // creates a <table> element and a <tbody> element
+    var div = document.createElement("sda");
     var tbl = document.createElement("table");
     var tblBody = document.createElement("tbody");
     
     var table_size = document.getElementById("myInput").value;
+
+    
+  
+    var div1 = document.getElementsByClassName("main");
+    
+    var questionRow = Math.floor(Math.random() * table_size)+1;   
+    var questionColumn = Math.floor(Math.random() * table_size)+1;   
+
+    var answerChoice;
+    var correctAnswer;
+
+    // alert(questionRow);
+    // alert(questionColumn);
 
     // creating all cells
     for (var i = 0; i <= table_size; i++) {
@@ -35,10 +41,18 @@ function generate_table() {
           }else{
             var cellText = document.createTextNode(i)
           }
+        }else if(i==questionRow && j==questionColumn){
+          var cellText = document. createElement( "INPUT" ); 
+          cellText. setAttribute( "type" , "text" );
+          cellText.style.fontSize = "30px";
+          answerChoice = cellText;
+          correctAnswer = i*j;
         }else{
           var cellText = document.createTextNode(i*j)
         }
         ;
+
+        cell.style.fontSize = "30px";
         cell.appendChild(cellText);
         row.appendChild(cell);
       }
@@ -46,13 +60,54 @@ function generate_table() {
       // add the row to the end of the table body
       tblBody.appendChild(row);
     }
-  
+
+
     // put the <tbody> in the <table>
     tbl.appendChild(tblBody);
     // appends <table> into <body>
     body.appendChild(tbl);
-    // sets the border attribute of tbl to 2;
     tbl.setAttribute("border", "2");
+
+    var div = document.createElement('div');
+    div.id = 'container';
+    div.className = 'border pad';
+  
+    document.body.appendChild(div);
+
+    // 1. Create the button
+    var button = document.createElement("button");
+    button.innerHTML = "Do Something";
+
+    // 2. Append somewhere
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+
+    // 3. Add event handler
+    button.addEventListener ("click", function() {
+      button.remove();
+      checkIfCorrect(answerChoice.value, correctAnswer, button);
+    });
+
+    
+  }
+
+  function checkIfCorrect(answerChoice, correctAnswer){
+    //Correct or Incorrect answer
+    var div = document.createElement('div');
+    
+    if (answerChoice == correctAnswer){
+      div.id = 'right';
+      div.innerHTML = 'Correct. Good Job!';
+    }else{
+      div.id = 'wrong';
+      div.innerHTML = 'Wrong. Try Again!';
+    }
+    
+    div.className = 'border pad';
+
+    // button.remove();
+    document.body.appendChild(div);
+    alert(cellText)
   }
 
 
